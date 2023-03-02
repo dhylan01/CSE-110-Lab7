8016be7451611702b30245cd3863cd022b38bf72
dhylan01 <dhylan01@gmail.com>
2023-03-02 15:03:21 -0800

    Update NoteRepository.java

--- app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteRepository.java
+++ app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteRepository.java
@@ -100,7 +100,6 @@ public class NoteRepository {
         if (clockFuture != null) {
             clockFuture.cancel(true);
         }
-        Note note = api.get(title);
         if (note != null) {
             upsertSynced(note);
         }
